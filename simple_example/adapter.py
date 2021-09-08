a73b0caf440d93b69d1aa19940ebd82593aac359
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2021-09-08 19:53:48 +0300

    Update adapter.py

--- simple_example/adapter.py
+++ simple_example/adapter.py
@@ -1,1 +1,29 @@
+class Ukrainian(object):
+    def __init__(self, name):
+        self._name = name
 
+    def greeting(self):
+        return '%s: Привіт!' % self._name
+
+
+class American(object):
+    def __init__(self, name):
+        self._name = name
+
+    def live_in(self):
+        return '%s: I live in America' % self._name
+
+
+class AmericanAdapter(Ukrainian):
+    def __init__(self, name):
+        super(AmericanAdapter, self).__init__(name=name)
+        self._american = American(name=name)
+
+    def greeting(self):
+        return self._american.live_in()
+
+
+person = Ukrainian('Ivan')
+print(person.greeting())
+person = AmericanAdapter('Joe')
+print(person.greeting())
