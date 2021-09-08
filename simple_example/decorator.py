ac81e04bba8809002fae22f4986c8cd2785cd78f
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2021-09-08 19:52:09 +0300

    Update decorator.py

--- simple_example/decorator.py
+++ simple_example/decorator.py
@@ -1,1 +1,24 @@
+class Person(object):
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print('Привіт! Я %s!' % self._name)
+
+
+class Ukrainian(object):
+    def __init__(self, man):
+        self._person = person
+
+    def __getattr__(self, item):
+        return getattr(self._person, item)
+
+    def go_to(self):
+        print('%s хоче відвідати Америку' % self._person._name)
+
+
+person = Person('Іван')
+person_ukrainian = Ukrainian(person)
+person_ukrainian.say()
+person_ukrainian.go_to()
 
