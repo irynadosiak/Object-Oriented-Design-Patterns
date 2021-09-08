5cea2fe1f86eb2452b83deda1aaec2092a56aae1
irynadosiak <43314418+irynadosiak@users.noreply.github.com>
2021-09-08 19:59:58 +0300

    Update visitor.py

--- simple_example/visitor.py
+++ simple_example/visitor.py
@@ -1,1 +1,42 @@
+class PersonVisitor(object):
+    def say(self, person):
+        methods = {
+            Ukrainian: self.say_ukrainian,
+        }
 
+        say = methods.get(type(person), self.say_unknown)
+        say(person)
+
+    def say_ukrainian(self, person):
+        print('Привіт!')
+
+    def say_unknown(self, person):
+        print('Hello!')
+
+
+class Person(object):
+    "Person"
+
+    def say(self, visitor):
+        visitor.say(self)
+
+
+class American(Person):
+    "American"
+
+
+class Ukrainian(Person):
+    "Ukrainian"
+
+
+class Italian(Person):
+    "Italian"
+
+
+visitor = PersonVisitor()
+ukrainian = Ukrainian()
+ukrainian.say(visitor)
+american = American()
+american.say(visitor)
+italian = Italian()
+italian.say(visitor)
